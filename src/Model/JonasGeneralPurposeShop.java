fe07fc0b31994b4f350a13236c742c4feb2085be
Enam1997 <89854906+Enam1997@users.noreply.github.com>
2021-08-31 21:16:36 +0600

    Initial commit

---
+++ src/Model/JonasGeneralPurposeShop.java
@@ -0,0 +1,18 @@
+
+package Model;
+
+import Controller.Controller;
+
+
+public class JonasGeneralPurposeShop {
+
+  
+    public static void main(String[] args) {
+        
+        Order order = new Order();
+        //order.deleteAllFromCart();
+          Controller con = new Controller();
+        con.run();
+    }
+    
+}
